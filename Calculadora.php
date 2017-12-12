63f6ba0a040ac029ef325305283b7950e1e6afa1
Leonardo Tumadjian <tumadjian@gmail.com>
2017-12-12 03:46:26 -0200

    Create Calculadora.php

---
+++ Calculadora.php
@@ -0,0 +1,43 @@
+<?php
+namespace App;
+
+/**
+ * 
+ * Desenvolvido por: Leonardo Tumadjian
+ **/
+class Calculadora
+{
+    // Acumulador de total
+    public $total;
+
+    public function __construct($inicial=0)
+    {
+        $this->total = $inicial;
+    }
+
+    public function somar($valor)
+    {
+        $this->total += $valor;
+        return $this;
+    }
+
+    public function subtrair($valor)
+    {
+        $this->total -= $valor;
+        return $this;
+    }
+
+    public function expoente($expoente)
+    {
+        if ($expoente == 0) {
+            throw new Exception("Expoente não pode ser 0");
+        }
+        $this->total **= $expoente;
+        return $this;
+    }
+
+    public function getTotal()
+    {
+        return $this->total;
+    }
+}
